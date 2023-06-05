53a3f55f7179df56bff186745c668a2c82df274a
ando <leo81@outlook.jp>
2023-06-06 08:41:48 +0900

    Step0: セットアップする

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
 
+import 'app.dart';
+
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      title: 'Haiku Generator Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: const HaikuPage(title: 'Haiku generator for Google products'),
     );
   }
 }
